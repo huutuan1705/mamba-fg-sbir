1835451e281bbb1bdcb3b871f066d0a118a3f70a
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-03-10 21:46:48 +0700

    update main

--- mamba_block/model.py
+++ mamba_block/model.py
@@ -28,6 +28,11 @@ class MambaModule(nn.Module):
         self.output_size = self.args.output_size
         self.dropout = self.args.dropout
         
+        # self.d_model = 2048
+        # self.n_layers = 2
+        # self.output_size = 64
+        # self.dropout = 0.1
+        
         config = MambaConfig(d_model=self.d_model, n_layers=self.n_layers)
         self.backbone = Mamba(config)
         self.head = MambaHead(d_model=self.d_model, 
@@ -39,12 +44,13 @@ class MambaModule(nn.Module):
             x: Input tensor of shape (batch_size, seq_len, channels) for embeddings
         """
         sequence_output = self.backbone(x)
-        # output = self.head(sequence_output)
-        
-        # return output # (N, 64)
+        sequence_output = sequence_output[:, -1, :]
         return F.normalize(sequence_output)
+        
+        # output = self.head(sequence_output)        
+        # return output # (N, 64)
     
-# model = MambaModule(d_model=2048, output_size=64)
+# model = MambaModule(None)
 # dummy_input = torch.randn(1, 25, 2048)
 # output = model(dummy_input)
 # print("Output shape:", output.shape) # (25, 64)
