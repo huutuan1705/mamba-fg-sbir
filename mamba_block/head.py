0b5ed69487f5065ea35b80f871e35b68bee71f97
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-03-11 22:47:03 +0700

    update model

--- mamba_block/head.py
+++ mamba_block/head.py
@@ -26,10 +26,10 @@ class MambaHead(nn.Module):
             Tensor of shape (batch_size, output_size)
         """
         # Average pooling over sequence length
-        # x = x.mean(dim=1)  # (batch_size, d_model)
+        x = x.mean(dim=1)  # (batch_size, d_model)
         
         # Max pooling over sequence length
-        x = torch.max(x, dim=1).values
+        # x = torch.max(x, dim=1).values
 
         # Apply layer norm and dropout
         # x = self.norm(x)
