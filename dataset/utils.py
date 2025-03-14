15bdfdfc81fb676aa07d21bdfa5eadfd184026a7
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-03-14 14:08:22 +0700

    update utils

--- dataset/utils.py
+++ dataset/utils.py
@@ -7,12 +7,12 @@ def get_transform(type):
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]
     
-    # if type == 'train':
-    #     transform_list.extend([
-    #         transforms.RandomHorizontalFlip(0.25),
-    #         transforms.RandomVerticalFlip(0.25),
-    #         transforms.RandomRotation(15),
-    #     ])
+    if type == 'train':
+        transform_list.extend([
+            transforms.RandomHorizontalFlip(0.25),
+            transforms.RandomVerticalFlip(0.25),
+            transforms.RandomRotation(15),
+        ])
         
     return transforms.Compose(transform_list)
     
